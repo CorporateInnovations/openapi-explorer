a49e85a4b3925aa912be227527024b5f48d8e9c6
Mohammed-CiGroup <133980765+Mohammed-CiGroup@users.noreply.github.com>
2023-07-18 15:07:50 +0100

    Updates

--- src/components/api-request.js
+++ src/components/api-request.js
@@ -475,7 +475,7 @@ export default class ApiRequest extends LitElement {
         <div class='request-body-container' data-selected-request-body-type="${this.selectedRequestBodyType}">
           <div class="table-title top-gap row">
             REQUEST BODY ${this.request_body.required ? html`<span class="mono-font" style='color:var(--red)'></span>` : ''} 
-            <span style = "font-weight:normal; margin-left:5px"> ${this.selectedRequestBodyType}</span>
+            <span style = "font-weight:normal; margin-left:5px; font-family: Courier New;"> ${this.selectedRequestBodyType}</span>
             <span style="flex:1"></span>
             ${reqBodyTypeSelectorHtml}
           </div> 
--- src/styles/font-styles.js
+++ src/styles/font-styles.js
@@ -23,6 +23,7 @@ export default css`
     font-family: var(--font-regular);
     font-size: calc(var(--font-size-small) + 4px);
     font-weight:bold;
+    color: black;
   }
   .tiny-title { 
     font-size:calc(var(--font-size-small) + 1px); 
